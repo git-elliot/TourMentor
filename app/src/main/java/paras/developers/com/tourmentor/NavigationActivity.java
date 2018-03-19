31ab8e52b55b7efa79f29c3b8a369109a2807220
parasthekoder <khandelwalparas8@gmail.com>
2018-03-20 00:12:14 +0530

    navigation drawer added

--- app/src/main/java/paras/developers/com/tourmentor/NavigationActivity.java
+++ app/src/main/java/paras/developers/com/tourmentor/NavigationActivity.java
@@ -40,6 +40,8 @@ public class NavigationActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
